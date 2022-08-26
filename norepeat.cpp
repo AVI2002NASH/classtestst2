80cdd8372007b20708ae0401f94775f000c2a046
AVI2002NASH <avinash0142.be20@chitkara.edu.in>
2022-08-26 10:13:17 +0530

    Add all my files

---
+++ norepeat.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+//main function
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){      //for loop to take array input from user
+        cin>>arr[i];
+    }
+    cout<<"non repeating numbers are:";
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        if(arr[i-1]!=arr[i] && arr[i]!=arr[i+1]){
+            cout<<arr[i]<<",";
+        }
+    }
+
+    return 0;
+}
